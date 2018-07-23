acfe395632385d57127c9e759a360e7a3e3a9bb2
JamieCovell <jamiec.covellj@gmail.com>
2018-07-23 18:51:08 +0100

    StartingMana

---
+++ Assets/Scripts/ManaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaController : MonoBehaviour {
+
+    public int speed;
+    public Image Bar;
+
+    private void Update ()
+    {
+        
+        
+
+    }
+
+}
